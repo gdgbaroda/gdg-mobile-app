cc2b3930060f1bcc8ba2a4c745940eca8cad6e5c
Sneh Pandya <sneh.pandya1@gmail.com>
2019-06-23 19:58:44 +0530

    Update meetup-auth.js

--- api/routes/meetup-auth.js
+++ api/routes/meetup-auth.js
@@ -8,8 +8,8 @@ let MeetupOAuth2Strategy = require('passport-oauth2-meetup').Strategy;
 mongoose.connect('mongodb://localhost:27017/raize', {useNewUrlParser: true});
 
 passport.use(new MeetupOAuth2Strategy({
-    clientID: '7h0rfilbk1h5bqmaun3rjv5vup',
-    clientSecret: '67cf4taabltpu080gsqfi9790c',
+    clientID: 'YOUR_CLIENT_ID_KEY_HERE',
+    clientSecret: 'YOUR_CLIENT_SECRET_KEY_HERE',
     callbackURL: "http://localhost/auth/meetup/callback",
     autoGenerateUsername: true
 }, function (accessToken, refreshToken, profile, done) {
@@ -25,8 +25,8 @@ router.get('/auth/meetup',
 router.get('/auth/meetup/callback', function (req, res) {
     axios.post('https://secure.meetup.com/oauth2/access', {}, {
         params: {
-            client_id: '7h0rfilbk1h5bqmaun3rjv5vup',
-            client_secret: 'co86t9hoi3lo58qatlu6c6277m',
+            client_id: 'YOUR_CLIENT_ID_KEY_HERE',
+            client_secret: 'YOUR_CLIENT_SECRET_KEY_HERE',
             grant_type: 'authorization_code',
             redirect_uri: 'http://localhost/auth/meetup/callback',
             code: req.query.code
